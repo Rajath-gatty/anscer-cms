4a10a17d1df3b60b5e6a321b4920294f97dd59cc
Rajath-gatty <rajathgatty253@gmail.com>
2026-07-15 14:40:28 +0530

    text corrections

--- app/about-us/_components/TeamSection.tsx
+++ app/about-us/_components/TeamSection.tsx
@@ -9,7 +9,7 @@ export function TeamSection() {
         eyebrow="The Team Behind the Tech"
         title="Built by"
         highlightedTitle="Believers"
-        copy="All the Answers You Need About Anscer RoboticsRight at Your Fingertips. From how our autonomous robots transform industries to what makes our tech stand outyour journey into the future of smart automation starts here."
+        copy="All the Answers You Need About ANSCER Robotics right at your Fingertips. From how our autonomous robots transform industries to what makes our tech stand out your journey into the future of smart automation starts here."
       />
     </section>
   );
